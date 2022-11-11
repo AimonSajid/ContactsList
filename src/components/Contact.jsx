e0b783467cd055370c9a1c885b487d284f846944
Aimon Sajid <saimonareej@gmail.com>
2022-11-11 02:37:01 -0800

    feat: Format contacts and added search for contacts list

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-const Contact = ({ contact }) => {
+const Contact = ({ contact, ids, setIds }) => {
+
+  const [checked, setChecked] = useState(false);
+
+  const addToIds = (id) => {
+    let idsArr = [...ids];
+    idsArr.push(id);
+    setIds(idsArr);
+  }
+
+  const removeFromIds = (id) => {
+    let idsArr = [...ids];
+    const index = idsArr.indexOf(id);
+    idsArr.splice(index, 1);
+    setIds(idsArr);
+  }
+
+  const onToggleCheckbox = (event, id) => {
+    setChecked(event.target.checked);
+    if (event.target.checked) {
+      addToIds(id);
+    } else {
+      removeFromIds(id);
+    }
+  }
+
+  useEffect(() => {
+    ids.some((id) => id == contact.id) && setChecked(true);
+  })
 
   return (
     <li>
-      {contact.first_name}
+      <div>
+        <img src={contact.avatar} />
+        {contact.first_name} {contact.last_name}
+        <input type="checkbox" checked={checked} onChange={(event) => onToggleCheckbox(event, contact.id)}/>
+      </div>
     </li>
   )
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from "react";
-import Contact from './Contact';
 import axios from "axios";
+import { USERS_URL } from "../constants/urls";
+import Contact from './Contact';
 
 const ContactList = () => {
 
   const [contacts, setContacts] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [ids, setIds] = useState([]);
+  const [nameToSearch, setNameToSearch] = useState('');
+
+  const sortByLastName = (a, b) => {
+    if (a.last_name < b.last_name) {
+      return -1;
+    }
+    if (a.last_name > b.last_name) {
+      return 1;
+    }
+    return 0;
+  }
 
   const fetchData = async () => {
-    await axios.get("https://teacode-recruitment-challenge.s3.eu-central-1.amazonaws.com/users.json")
+    await axios.get(USERS_URL)
       .then(res => {
         if (res.data) {
-          setContacts(res.data);
+          setContacts(res.data.sort((a, b) => sortByLastName(a, b)));
+          setFiltered(res.data.sort((a, b) => sortByLastName(a, b)));
         }
       });
   }
@@ -19,11 +34,22 @@ const ContactList = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    console.log('ids : ', ids);
+  }, [ids]);
+
+  useEffect(() => {
+    const newContacts = contacts.filter(contact => contact.first_name.toLowerCase().includes(nameToSearch.toLowerCase())
+                          || contact.last_name.toLowerCase().includes(nameToSearch.toLowerCase()))
+    setFiltered(newContacts);
+  }, [nameToSearch])
+
   return (
     <div>
+      Search : <input type='text' value={nameToSearch} onChange={(event) => setNameToSearch(event.target.value)} />
       <ul>
-        {contacts.map((contact) => {
-          return <Contact contact={contact} />
+        {filtered.map((contact) => {
+          return <Contact key={contact.id} ids={ids} setIds={setIds} contact={contact} />
         })}
       </ul>
     </div>
